816c1bba254ed29f8a91e697f4752ae8ef6e3f03
Brooke Kuhlmann <brooke@alchemists.io>
2016-05-19 17:44:59 -0600

    Updated to Alfred 3.

--- settings/settings.sh
+++ settings/settings.sh
@@ -140,8 +140,8 @@ export TABLE_EDITOR_EXTENSION_URL="https://github.com/vkocubinsky/SublimeTableEd
 export SUBLIME_URL_HANDLER_APP_NAME="SublHandler.app"
 export SUBLIME_URL_HANDLER_APP_URL="https://github.com/downloads/asuth/subl-handler/SublHandler.zip"
 
-export ALFRED_APP_NAME="Alfred 2.app"
-export ALFRED_APP_URL="https://cachefly.alfredapp.com/Alfred_2.8.4_437.zip"
+export ALFRED_APP_NAME="Alfred 3.app"
+export ALFRED_APP_URL="https://cachefly.alfredapp.com/Alfred_3.0.1_671.zip"
 
 export TEXTEXPANDER_APP_NAME=TextExpander.app
 export TEXTEXPANDER_APP_URL="http://dl.smilesoftware.com/com.smileonmymac.textexpander/TextExpander.zip"
