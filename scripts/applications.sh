e86ee6663c5dac6fc62038d914c1658b23890e11
Niall Murphy <niallm@avernus.net>
2016-07-03 22:48:56 +0100

    Remove his ones

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -7,8 +7,8 @@
 # Dropbox
 install_dmg_app "$DROPBOX_APP_URL" "Dropbox Installer" "$DROPBOX_APP_NAME"
 
-# Knox
-install_zip_app "$KNOX_APP_URL" "$KNOX_APP_NAME"
+# # Knox
+# install_zip_app "$KNOX_APP_URL" "$KNOX_APP_NAME"
 
 # iTerm
 install_zip_app "$ITERM_APP_URL" "$ITERM_APP_NAME"
@@ -19,17 +19,17 @@ install_dmg_app "$SUBLIME_TEXT_APP_URL" "Sublime Text" "$SUBLIME_TEXT_APP_NAME"
 # Sublime Text URL Handler
 install_zip_app "$SUBLIME_URL_HANDLER_APP_URL" "$SUBLIME_URL_HANDLER_APP_NAME"
 
-# Alfred
-install_zip_app "$ALFRED_APP_URL" "$ALFRED_APP_NAME"
+# # Alfred
+# install_zip_app "$ALFRED_APP_URL" "$ALFRED_APP_NAME"
 
-# TextExpander
-install_zip_app "$TEXTEXPANDER_APP_URL" "$TEXTEXPANDER_APP_NAME"
+# # TextExpander
+# install_zip_app "$TEXTEXPANDER_APP_URL" "$TEXTEXPANDER_APP_NAME"
 
 # Path Finder
 install_zip_app "$PATH_FINDER_APP_URL" "$PATH_FINDER_APP_NAME"
 
-# Tor Browser
-install_dmg_app "$TOR_BROWSER_APP_URL" "Tor Browser" "$TOR_BROWSER_APP_NAME"
+# # Tor Browser
+# install_dmg_app "$TOR_BROWSER_APP_URL" "Tor Browser" "$TOR_BROWSER_APP_NAME"
 
 # Google Chrome
 install_dmg_app "$CHROME_APP_URL" "Google Chrome" "$CHROME_APP_NAME"
@@ -40,35 +40,35 @@ install_dmg_app "$FIREFOX_APP_URL" "Firefox" "$FIREFOX_APP_NAME"
 # Opera
 install_dmg_app "$OPERA_APP_URL" "Opera" "$OPERA_APP_NAME"
 
-# Vivaldi
-install_dmg_app "$VIVALDI_APP_URL" "Vivaldi 1.1.453.47" "$VIVALDI_APP_NAME"
+# # Vivaldi
+# install_dmg_app "$VIVALDI_APP_URL" "Vivaldi 1.1.453.47" "$VIVALDI_APP_NAME"
 
-# OmniGraffle
-install_dmg_app "$OMNIGRAFFLE_APP_URL" "OmniGrafflePro" "$OMNIGRAFFLE_APP_NAME"
+# # OmniGraffle
+# install_dmg_app "$OMNIGRAFFLE_APP_URL" "OmniGrafflePro" "$OMNIGRAFFLE_APP_NAME"
 
 # Transmit
 install_zip_app "$TRANSMIT_APP_URL" "$TRANSMIT_APP_NAME"
 
-# iStat Menus
-install_zip_app "$ISTATS_APP_URL" "$ISTATS_APP_NAME"
+# # iStat Menus
+# install_zip_app "$ISTATS_APP_URL" "$ISTATS_APP_NAME"
 
-# Bartender
-install_zip_app "$BARTENDER_APP_URL" "$BARTENDER_APP_NAME"
+# # Bartender
+# install_zip_app "$BARTENDER_APP_URL" "$BARTENDER_APP_NAME"
 
-# DoubleTake
-install_zip_app "$DOUBLETAKE_APP_URL" "$DOUBLETAKE_APP_NAME"
+# # DoubleTake
+# install_zip_app "$DOUBLETAKE_APP_URL" "$DOUBLETAKE_APP_NAME"
 
-# ImageOptim
-install_tar_app "$IMAGE_OPTIM_APP_URL" "$IMAGE_OPTIM_APP_NAME" "-xjf"
+# # ImageOptim
+# install_tar_app "$IMAGE_OPTIM_APP_URL" "$IMAGE_OPTIM_APP_NAME" "-xjf"
 
 # VLC
 install_dmg_app "$VLC_APP_URL" "vlc-2.2.2" "$VLC_APP_NAME"
 
-# PG Admin
-install_dmg_app "$PG_ADMIN_APP_URL" "pgAdmin3" "$PG_ADMIN_APP_NAME"
+# # PG Admin
+# install_dmg_app "$PG_ADMIN_APP_URL" "pgAdmin3" "$PG_ADMIN_APP_NAME"
 
-# PSequel
-install_zip_app "$PSEQUEL_APP_URL" "$PSEQUEL_APP_NAME"
+# # PSequel
+# install_zip_app "$PSEQUEL_APP_URL" "$PSEQUEL_APP_NAME"
 
 # CheatSheet
 install_zip_app "$CHEATSHEET_APP_URL" "$CHEATSHEET_APP_NAME"
@@ -76,20 +76,20 @@ install_zip_app "$CHEATSHEET_APP_URL" "$CHEATSHEET_APP_NAME"
 # OpenOffice
 install_dmg_app "$OPEN_OFFICE_APP_URL" "OpenOffice" "$OPEN_OFFICE_APP_NAME"
 
-# Cloak
-install_dmg_app "$CLOAK_APP_URL" "Cloak" "$CLOAK_APP_NAME"
+# # Cloak
+# install_dmg_app "$CLOAK_APP_URL" "Cloak" "$CLOAK_APP_NAME"
 
-# SurfEasy VPN
-install_dmg_app "$SURF_EASY_VPN_APP_URL" "SurfEasy VPN" "$SURF_EASY_VPN_APP_NAME"
+# # SurfEasy VPN
+# install_dmg_app "$SURF_EASY_VPN_APP_URL" "SurfEasy VPN" "$SURF_EASY_VPN_APP_NAME"
 
 # VirtualBox
 install_dmg_pkg "$VIRTUAL_BOX_APP_URL" "VirtualBox" "$VIRTUAL_BOX_APP_NAME"
 
-# Doxie
-install_dmg_app "$DOXIE_APP_URL" "Doxie" "$DOXIE_APP_NAME"
+# # Doxie
+# install_dmg_app "$DOXIE_APP_URL" "Doxie" "$DOXIE_APP_NAME"
 
-# Sonos
-install_dmg_app "$SONOS_APP_URL" "Sonos" "$SONOS_APP_NAME"
+# # Sonos
+# install_dmg_app "$SONOS_APP_URL" "Sonos" "$SONOS_APP_NAME"
 
 # App Cleaner
 install_zip_app "$APP_CLEANER_APP_URL" "$APP_CLEANER_APP_NAME"
