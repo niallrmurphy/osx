ffaaa2369619d6a5040822c7fed16fb068f0680e
Niall Murphy <niallm@avernus.net>
2016-07-10 02:06:07 +0100

    Appstore stuff

---
+++ scripts/appstore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# DESCRIPTION
+# Installs OSX Mac Store applications.
+
+mas install 456877552 # GeekTool
+mas install 497799835 # Xcode
+mas install 419330170 # Moom
+mas install 405399194 # Kindle
+mas install 409201541 # Pages
+mas install 525912054 # WiFi Signal
+mas install 654667344 # The Duplicate Finder
+mas install 514951692 # NetSpot
+mas install 409789998 # Twitter
+mas install 425424353 # The Unarchiver
+mas install 407963104 # Pixelmator
+mas install 456877552 # GeekTool
+mas install 409203825 # Numbers
+mas install 676490829 # iNetGrapher
+mas install 409183694 # Keynote
+mas install 408980954 # GarageBand
+mas install 672206759 # Disk Diag
+mas install 425264550 # Blackmagic Disk Speed Test
+mas install 499233976 # Cathode
+mas install 408981434 # iMovie
+mas install 926036361 # LastPass
+mas install 477991355 # DupeZap Plus
